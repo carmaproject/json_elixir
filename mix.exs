b98992b68488171063f62c55e91f132de3c0ed79
hassanRsiddiqi <hassan.raza@carmaproject.com>
2021-07-16 16:29:54 +0500

    JSON to HTML

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule JsonElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/carmaproject/json_elixir"
     ]
   end
 
@@ -21,8 +24,21 @@ defmodule JsonElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:json, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  def package do
+    [
+      name: "ex_json",
+      maintainers: "hassanRSiddiqi",
+      organization: "carmaproject",
+      licenses: ["MIT License"],
+    ]
+  end
+
+  defp description() do
+    "convert raw json into html."
   end
 end
--- test/json_elixir_test.exs
+++ test/json_elixir_test.exs
@@ -2,7 +2,34 @@ defmodule JsonElixirTest do
   use ExUnit.Case
   doctest JsonElixir
 
-  test "greets the world" do
-    assert JsonElixir.hello() == :world
-  end
+  test "to_html/1 invalid input nil", do:
+    assert JsonElixir.to_html(nil) == {:error, "expected list or bit string got: nil"}
+
+  test "to_html!/1 invalid input nil", do:
+    assert_raise(ArgumentError, fn -> JsonElixir.to_html!(nil) end)
+
+  test "to_html/1 invalid input %{}", do:
+    assert JsonElixir.to_html(%{}) == {:error, "expected list or bit string got: %{}"}
+
+  test "to_html!/1 invalid input %{}", do:
+    assert_raise(ArgumentError, fn -> JsonElixir.to_html!(%{}) end)
+
+  test "to_html/1 invalid input []", do:
+    assert JsonElixir.to_html([]) == {:error, :unexpected_end_of_buffer}
+
+  test "to_html!/1 invalid input []", do:
+    assert_raise(ArgumentError, fn -> JsonElixir.to_html!([]) end)
+
+  test "to_html/1 valid input", do:
+    assert JsonElixir.to_html(~s(
+      {
+        "headers": {
+          "Content-Type": "application/json"
+        }
+      }
+    )) == {:ok, "<table><tr><td></td><td>%{</td></tr><tr><td></td><td></td><td>headers:</td><td><tr><td></td><td>%{</td></tr><tr><td></td><td></td><td>Content-Type:</td><td>application/json,</td></tr><tr><td></td><td>}%</td></tr></td></tr><tr><td></td><td>}%</td></tr></table>"}
+
+  test "to_html!/1 input", do:
+    assert JsonElixir.to_html!(~s({"a": 1})) == "<table><tr><td></td><td>%{</td></tr><tr><td></td><td></td><td>a:</td><td>1,</td></tr><tr><td></td><td>}%</td></tr></table>"
+
 end
